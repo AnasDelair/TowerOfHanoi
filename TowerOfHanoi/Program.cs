35d0e79387603054d2c4a7e8d9237391ce65e3cc
Anas Delair <24ADelair@heckgrammar.co.uk>
2024-11-22 14:15:13 +0000

    update 2

--- TowerOfHanoi/Program.cs
+++ TowerOfHanoi/Program.cs
@@ -2,6 +2,17 @@
 {
     internal class Program
     {
+        public static class Game
+        {
+            public static int moves = 0;
+            public static int gameEnded = 0;
+            public const int TowerNumber = 3;
+            public const int DiskNumber = 3;
+            public const int startingTower = 0;
+            public const int maxSize = DiskNumber * 2;
+            public const int maxHeight = DiskNumber + 1;
+        }
+
         struct Disk
         {
             public int Tower;
@@ -15,28 +26,22 @@
                 Size = diskSize * 2;
             }
         }
+
         static void Main(string[] args)
         {
             List<Stack<Disk>> Towers = new List<Stack<Disk>>();
 
-            Towers.Add(new Stack<Disk>());
-            Towers.Add(new Stack<Disk>());
-            Towers.Add(new Stack<Disk>());
+            for (int i = 1; i <= Game.TowerNumber; i++)
+            {
+                Towers.Add(new Stack<Disk>());
+            }
 
-            Stack<int> teststack = new Stack<int>();
-            teststack.Push(0);
+            for (int i = 1; i <= Game.DiskNumber; i++)
+            {
+                Towers[Game.startingTower].Push(new Disk((Game.DiskNumber + 1) - i, i));
+            }
 
-            Disk Disk1 = new Disk(3, 1);
-            Disk Disk2 = new Disk(2, 2);
-            Disk Disk3 = new Disk(1, 3);
-
-            Towers[Disk1.Tower].Push(Disk1);
-            Towers[Disk2.Tower].Push(Disk2);
-            Towers[Disk3.Tower].Push(Disk3);
-
-            Disk3 = moveDisk(Towers, Disk3, 1);
-
-            // Console.WriteLine((Towers[Disk3.Tower].Peek().Size == Disk3.Size));
+            solveHanoi(Towers);
         }
 
         static bool isEmpty(Stack<Disk> stack)
@@ -61,7 +66,7 @@
                     Console.WriteLine("Trying to move a disk that isnt at the top of the tower.");
                     return false; // disk you are moving has to be the topmost disk
                 }
-                if (currentDisk.Size > disk.Size)
+                if (currentDisk.Size < disk.Size)
                 {
                     Console.WriteLine("Trying to move the disk to a tower with a smaller disk on it.");
                     return false; // size check
@@ -83,11 +88,35 @@
             return false;
         }
 
-        static Disk moveDisk(List<Stack<Disk>> Towers, Disk disk, int towerIndex)
+        static void moveDisk(List<Stack<Disk>> Towers, int diskID, int towerIndex)
         {
+            Disk disk = new Disk();
+            bool found = false;
+            foreach (Stack<Disk> Tower in Towers)
+            {
+                if (found) break;
+                foreach (Disk thisDisk in Tower)
+                {
+                    if (thisDisk.Size == diskID * 2)
+                    {
+                        disk = thisDisk;
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Desired disk to move couldn't be found.");
+                return;
+            }
+
             if (isValid(Towers, disk, towerIndex))
             {
                 Console.WriteLine("Valid Move!");
+                Game.moves += 1;
+                Console.WriteLine($"Moves made: {Game.moves}");
 
                 Disk newDisk = new Disk(disk.Size / 2, disk.HeightIndex);
                 newDisk.Tower = towerIndex;
@@ -96,26 +125,56 @@
                 Towers[towerIndex].Push( newDisk ); // take out the current disk, and put it in the new tower
 
                 Console.WriteLine($"Disk {newDisk.Size / 2} moved to tower {newDisk.Tower}!");
+                disk = newDisk;
 
                 displayHanoi(Towers);
-                return newDisk;
+                return;
             }
             Console.WriteLine("Invalid move!");
-            return disk;
         }
 
         static void displayHanoi(List<Stack<Disk>> Towers)
         {
+            int maxSize = Game.maxSize;
+            int maxHeight = Game.maxHeight;
+
+            Console.WriteLine();
             foreach (Stack<Disk> Tower in Towers)
             {
-                Console.WriteLine();
-                foreach (Disk disk in Tower)
+                if (Tower.Count > 0)
                 {
-                    string diskString = "<" + new string('=', disk.Size) + ">";
-                    diskString = diskString.Insert(diskString.Length / 2, "|");
-                    diskString = new string(' ', disk.Size) + diskString;
-                    Console.WriteLine(diskString);
+                    for (int i = 0; i <= maxHeight - Tower.Count; i++)
+                    {
+                        Console.WriteLine(new string(' ', maxSize + 1) + "|");
+                    }
+                    foreach (Disk disk in Tower)
+                    {
+                        string diskString = "<" + new string('=', disk.Size) + ">";
+                        diskString = diskString.Insert(diskString.Length / 2, "|");
+                        diskString = new string(' ', maxSize - disk.Size / 2) + diskString;
+                        Console.WriteLine(diskString);
+                    }
+                    Console.WriteLine(new string('-', maxSize * 2 + maxSize / 2));
+                    Console.WriteLine();
+                } else
+                {
+                    for (int i = 0; i <= maxHeight; i++)
+                    {
+                        Console.WriteLine(new string(' ', maxSize + 1) + "|");
+                    }
+                    Console.WriteLine(new string('-', maxSize * 2 + maxSize / 2));
+                    Console.WriteLine();
                 }
+            }
+        }
+
+        static void solveHanoi(List<Stack<Disk>> Towers)
+        {
+            if (Game.DiskNumber % 2 == 1)
+            {
+                Console.WriteLine("Solving Hanoi Even Method");
+                if Game.moves
+                moveDisk(Towers, 1, 2);
             }
         }
 
